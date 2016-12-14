30ee885b521ac4307fbd23cce3ba11a8df986a51
Daniel Persson <mailto.woden@gmail.com>
2016-12-14 21:47:09 +0100

    First implementation.

---
+++ index.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+console.log('Tool is still under development, if this is what you\'ve been looking for then join the team at https://github.com/kalaspuffar/polymer-dependency-weight');
